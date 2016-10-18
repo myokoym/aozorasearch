e8b63064d1ef4d9e491b58915c32a3dbb62212a7
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-10-19 02:51:41 +0900

    Add missing require for web/app.rb

--- lib/aozorasearch.rb
+++ lib/aozorasearch.rb
@@ -3,6 +3,7 @@ require "aozorasearch/groonga_database"
 require "aozorasearch/groonga_searcher"
 require "aozorasearch/loader"
 require "aozorasearch/book"
+require "aozorasearch/command"
 
 module Aozorasearch
 end
